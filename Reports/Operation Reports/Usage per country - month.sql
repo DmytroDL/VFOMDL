b72855f49729394bdb63de605c726e0baaf9005d
Dmytro Mazhar <dmytro.mazhar@netcracker.com>
2022-04-20 15:17:22 +0400

    fix reports
    fix reports

--- Reports/Operation Reports/Usage per country - month.sql
+++ Reports/Operation Reports/Usage per country - month.sql
@@ -1,4 +1,5 @@
 with all_calls as (select
+originating_country,
 calling_number,
 case
 when calling_number like '1%' THEN substr(calling_number,1,1)
@@ -27,6 +28,7 @@ when calling_number like '99%' THEN substr(calling_number,1,3)
 ELSE substr(calling_number,1,2) END cfrm,
 called_number,
 case
+when length(called_number) = 5 THEN 'PDS_NUMBER'
 when called_number like '1%' THEN substr(called_number,1,1)
 when called_number like '7%' THEN substr(called_number,1,1)
 when called_number like '21%' THEN substr(called_number,1,3)
@@ -50,43 +52,43 @@ when called_number like '88%' THEN substr(called_number,1,3)
 when called_number like '96%' THEN substr(called_number,1,3)
 when called_number like '97%' THEN substr(called_number,1,3)
 when called_number like '99%' THEN substr(called_number,1,3)
+when called_number like '0091%' THEN '91'
 ELSE substr(called_number,1,2) END cto,
-duration_seconds,
-number_of_messages,
+duration_seconds/60 duration,
+number_of_messages sms,
 date_format(event_dtm,'MMMM') event_date,
 record_type_value
 from refined.fct_prepaid_usages
-where record_type_value in ('Voice','SMS')),
+where record_type_value in ('Voice','SMS')
+and `date` = '2022-04-20'),
 out_events as (select 
 ac.event_date event_date,
-cc.country originating_country,
+ac.originating_country originating_country,
 cc2.country destination_country,
-sum(if(ac.record_type_value = 'Voice', 0.0, 0.0)/60) as In_Mins,
-sum(if(ac.record_type_value = 'Voice', nvl(ac.duration_seconds,0.0), 0)/60) as Out_Mins,
+sum(if(ac.record_type_value = 'Voice', 0.0, 0.0)) as In_Mins,
+sum(if(ac.record_type_value = 'Voice', nvl(ac.duration,0.0), 0)) as Out_Mins,
 sum(if(ac.record_type_value = 'SMS', 0.0, 0.0)) as In_SMS,
-sum(if(ac.record_type_value = 'SMS', nvl(ac.number_of_messages,0.0), 0)) as Out_SMS,
+sum(if(ac.record_type_value = 'SMS', nvl(ac.sms,0.0), 0)) as Out_SMS,
 count(distinct ac.calling_number) msisdn_cnt,
 sum(if(ac.record_type_value = 'Voice', 1.0, 0.0)) as calls_cnt
 from all_calls ac
-left join datamart.dm_country_codes cc on cc.code = ac.cfrm
 left join datamart.dm_country_codes cc2 on cc2.code = ac.cto
-where ac.cfrm = '968' and ac.cto <> '968'
-group by event_date,cc.country,cc2.country),
+where ac.cfrm = '968' and ac.cto <> '968' and ac.originating_country = 'Oman' and ac.cto <> 'PDS_NUMBER'
+group by event_date,ac.originating_country,cc2.country),
 inc_events as (select 
 ac.event_date event_date,
-cc.country originating_country,
+ac.originating_country originating_country,
 cc2.country destination_country,
-sum(if(ac.record_type_value = 'Voice', nvl(ac.duration_seconds,0.0), 0)/60) as In_Mins,
-sum(if(ac.record_type_value = 'Voice', 0.0, 0.0)/60) as Out_Mins,
-sum(if(ac.record_type_value = 'SMS', nvl(ac.number_of_messages,0.0), 0)) as In_SMS,
+sum(if(ac.record_type_value = 'Voice', nvl(ac.duration,0.0), 0)) as In_Mins,
+sum(if(ac.record_type_value = 'Voice', 0.0, 0.0)) as Out_Mins,
+sum(if(ac.record_type_value = 'SMS', nvl(ac.sms,0.0), 0)) as In_SMS,
 sum(if(ac.record_type_value = 'SMS', 0.0, 0.0)) as Out_SMS,
 count(distinct ac.calling_number) msisdn_cnt,
 sum(if(ac.record_type_value = 'Voice', 1.0, 0.0)) as calls_cnt
 from all_calls ac
-left join datamart.dm_country_codes cc on cc.code = ac.cfrm
 left join datamart.dm_country_codes cc2 on cc2.code = ac.cto
-where ac.cfrm <> '968' and ac.cto = '968'
-group by event_date,cc.country,cc2.country)
+where ac.cfrm <> '968' and ac.cto = '968' and ac.originating_country <> 'Oman'
+group by event_date,ac.originating_country,cc2.country)
 select * from out_events
-union all
+union
 select * from inc_events
